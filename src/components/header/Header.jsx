dcf746a63fe2f155efa2ee12b21fdf2ead0afeb2
olaniyi <arokoyueb11@gmail.com>
2025-01-30 22:14:14 +0100

    finishing up

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import './header.css'
-import BlurText from '../../TextAnimations/BlurText/BlurText'
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 const Header = () => {
+
+    const [ref, InView] = useInView({
+        triggerOnce: false,
+        threshold: 0.2,
+    });
+    
   return (
       <div className='header'>
           <div className='checker_bg_container'>
@@ -9,10 +17,15 @@ const Header = () => {
           </div>
           <div className='sub_header'>
               <div className='header_hero_text_container'>
-                  <div className='sub_header_hero_text_container'>
+                  <motion.div
+                  ref={ref}
+                  initial={{ opacity: 0, y: 0 }}
+                  animate={InView ? { opacity: 1, y: 20 } : {}}
+                  exit={{ opacity: 0, x: 20 }}
+                  transition={{ duration: 0.5, delay: 0.6 }}    className='sub_header_hero_text_container'>
                   <span className='header_hero_span'>let's help you stay</span>
                       <h1>Secured</h1>
-                  </div>
+                  </motion.div>
           
               <p className='header_hero_p'>Expert cybersecurity solutions tailored to protect <br /> your organization from emerging threats</p>
           </div>
