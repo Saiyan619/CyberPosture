fc9a3abad17e01bc43ecd167d17ef41ec22ed08d
olaniyi <arokoyueb11@gmail.com>
2025-02-01 06:00:41 +0100

    finishing up header/Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,23 +1,41 @@
 import React from 'react'
 import { useState } from 'react'
 import Hamburger from 'hamburger-react';
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import './nav.css'
 
 const Nav = () => {
     const [change, setChange] = useState(false)
+      const [ref, InView] = useInView({
+        triggerOnce: false,
+        threshold: 0.2,
+      });
+    
     function toggleNav() {
         setChange(!change)
     }
 
 
   return (
-      <nav>
+      <nav >
           
-          <div>
+          <motion.div
+              ref={ref}
+              initial={{ opacity: 0, y: 0 }}
+              animate={InView ? { opacity: 1, y: 2 } : {}}
+              exit={{ opacity: 0, x: 20 }}
+              transition={{ duration: 0.5, delay: 0.6 }}>
               <img src="./Group (4).png" alt="nav logo" />
-          </div>
+          </motion.div>
             
-          <div className={!change ? 'sub_nav_mobile' : 'sub_nav_mobile_active'}>
+          <motion.div
+            ref={ref}
+            initial={{ opacity: 0, y: 0 }}
+            animate={InView ? { opacity: 1, y: 2 } : {}}
+            exit={{ opacity: 0, x: 20 }}
+        transition={{ duration: 0.5, delay: 0.6 }}
+        className={!change ? 'sub_nav_mobile' : 'sub_nav_mobile_active'}>
           <ul>
               <li>Services</li>
               <li>About</li>
@@ -27,7 +45,7 @@ const Nav = () => {
           <div>
               <button className='nav_btn'>Schedule a call</button>
           </div>  
-          </div>
+          </motion.div>
       
 
           <div onClick={toggleNav} className="hamburger">
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,43 +1,46 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
 const Header = () => {
+  const [ref, InView] = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
 
-    const [ref, InView] = useInView({
-        triggerOnce: false,
-        threshold: 0.2,
-    });
-    
   return (
-      <div className='header'>
-          <div className='checker_bg_container'>
-              <img className='checker_bg' src="./checkered-bg.png" alt="checker_bg" />
-          </div>
-          <div className='sub_header'>
-              <div className='header_hero_text_container'>
-                  <motion.div
-                  ref={ref}
-                  initial={{ opacity: 0, y: 0 }}
-                  animate={InView ? { opacity: 1, y: 20 } : {}}
-                  exit={{ opacity: 0, x: 20 }}
-                  transition={{ duration: 0.5, delay: 0.6 }}    className='sub_header_hero_text_container'>
-                  <span className='header_hero_span'>let's help you stay</span>
-                      <h1>Secured</h1>
-                  </motion.div>
-          
-              <p className='header_hero_p'>Expert cybersecurity solutions tailored to protect <br /> your organization from emerging threats</p>
-          </div>
+    <div className="header">
+      <div className="checker_bg_container">
+        <img className="checker_bg" src="./checkered-bg.png" alt="checker_bg" />
+      </div>
+      <div className="sub_header">
+        <div className="header_hero_text_container">
+          <motion.div
+            ref={ref}
+            initial={{ opacity: 0, y: 0 }}
+            animate={InView ? { opacity: 1 } : {}}
+            exit={{ opacity: 0}}
+            transition={{ duration: 0.5, delay: 0.6 }}
+            className="sub_header_hero_text_container"
+          >
+            <span className="header_hero_span">let's help you stay</span>
+            <h1>Secured</h1>
+          </motion.div>
 
-          <div className='header_hero_btns_container'>
-              <button className='header_btn_1'>Schedule a call</button>
-              <button className='header_btn_2'>Get a Quote</button>
-          </div>
-          </div>
+          <p className="header_hero_p">
+            Expert cybersecurity solutions tailored to protect <br /> your
+            organization from emerging threats
+          </p>
+        </div>
 
+        <div className="header_hero_btns_container">
+          <button className="header_btn_1">Schedule a call</button>
+          <button className="header_btn_2">Get a Quote</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
